ab34a261cc72351487bec6c287bf7f60267503f6
Julia Naomi <jnaomi@thoughtworks.com>
2018-06-03 02:59:03 -0300

    Creates edn-maps to json parsers

--- src/api_job/handlers/language.clj
+++ src/api_job/handlers/language.clj
@@ -1,4 +1,11 @@
-(ns api-job.handlers.language)
+(ns api-job.handlers.language
+  (:require [cheshire.core :as json]))
+
+(defn edns-to-json [m1 m2]
+  (let [merged-map (merge m1 m2)]
+    (json/generate-string merged-map)))
 
 (defn browse-by-lang [lang]
-  {:language lang :jobs []})
+  (let [m1 {:language lang :jobs []}
+        m2 {}]
+    (edns-to-json m1 m2)))
--- test/api_job/handlers/language_test.clj
+++ test/api_job/handlers/language_test.clj
@@ -1,9 +1,16 @@
 (ns api-job.handlers.language-test
   (:require [clojure.test :refer :all]
+            [cheshire.core :as json]
             [api-job.handlers.language :refer :all]))
 
 (deftest language-handler-testing
   (testing "browse-by-lang"
-    (let [response (browse-by-lang "clojure")]
+    (let [client-response (browse-by-lang "clojure")
+          response (json/parse-string client-response true)]
       (is (= (:language response) "clojure"))
-      (is (vector? (:jobs response))))))
+      (is (vector? (:jobs response)))))
+  (testing "edns-to-json"
+    (is (= (edns-to-json {:language "clojure" :jobs ["a" "b"]} {})
+           "{\"language\":\"clojure\",\"jobs\":[\"a\",\"b\"]}"))
+    (is (= (edns-to-json {:language "clojure" :jobs ["a" "b"]} {:jobs ["c"]}))
+        "{\"language\":\"clojure\",\"jobs\":[\"a\",\"b\",\"c\"]}")))
